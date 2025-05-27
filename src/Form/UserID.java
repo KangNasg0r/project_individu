f1ddf9e6ef3fb1e36350d6348b0f18a7a673ba71
Ahmad Nur Latif P <Ahmad Nur Latif P@VIVOBOOK-PRO14>
2025-05-27 20:48:51 +0700

    Update

--- src/Form/UserID.java
+++ src/Form/UserID.java
@@ -5,16 +5,16 @@ public class UserID {
     private static String id_kasir;
     private static String nama_kasir;
 
-    static void setIdKasir(String idTek) {
-        UserID.id_kasir = idTek;
+    static void setIdKasir(String idKasir) {
+        UserID.id_kasir = idKasir;
     }
 
     public static String getIdKasir() {
         return id_kasir;
     }
 
-    public static void setNamaKasir(String namaTeknisi) {
-        UserID.nama_kasir = namaTeknisi;
+    public static void setNamaKasir(String namaKasir) {
+        UserID.nama_kasir = namaKasir;
     }
 
     public static String getNamaKasir() {
